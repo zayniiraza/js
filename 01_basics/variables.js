d72b7d02caa6372e8f6e67bc289d1085fd0d7845
zayniiraza <135531985+zayniiraza@users.noreply.github.com>
2024-04-30 19:40:49 +0000

    variables and extensions

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const ACCOUNTID = 2008;
+let accountEmail = "zayn@google.com";
+var accountPassword = "12345";
+accountCity = "Multan";
+// console.log(`${ACCOUNTID}
+// ${accountEmail}
+// ${accountPassword}
+// ${accountCity}`);
+
+console.table([ACCOUNTID, accountEmail, accountPassword, accountCity]);
